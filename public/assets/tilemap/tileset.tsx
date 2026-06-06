23ad83611f9a6b41965136a07c59a6ed8f591a7d
mica cordoba <micu.cordoba80@gmail.com>
2026-06-06 18:52:27 -0300

    arreglo del mapa 3

---
+++ public/assets/tilemap/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="tileset" tilewidth="32" tileheight="32" tilecount="484" columns="22">
+ <image source="../map3.png" width="720" height="720"/>
+</tileset>
