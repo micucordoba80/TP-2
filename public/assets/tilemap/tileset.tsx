6743edca59f291f960641ee73b09e7cb65c6b193
mica cordoba <micu.cordoba80@gmail.com>
2026-06-06 19:32:04 -0300

    quilombo q se me armo con los mapas ups

--- public/assets/tilemap/tileset.tsx
+++ public/assets/tilemap/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.2" name="tileset" tilewidth="32" tileheight="32" tilecount="484" columns="22">
- <image source="../map3.png" width="720" height="720"/>
+<tileset version="1.10" tiledversion="1.12.2" name="tileset" tilewidth="32" tileheight="32" tilecount="2500" columns="50">
+ <image source="../map3.png" width="1600" height="1600"/>
 </tileset>
